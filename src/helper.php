68e84d916c46c6741a0949efde346a69c90964be
lilwil <lilwil@163.com>
2016-11-02 09:45:49 +0800

    修正错误

--- src/helper.php
+++ src/helper.php
@@ -1,25 +1,23 @@
 <?php
 use think\Route;
-use think\Session;
 use think\Config;
+use think\geetest\GeetestLib;
+use think\Session;
 use think\Url;
-\think\Route::rule('geetest/[:id]', "\\think\\geetest\\GeetestController@index");
+// 注册路由
+Route::rule('geetest/[:id]', "\\think\\geetest\\GeetestController@index");
 
-/**
- *
- * @return bool
- */
 function geetest($config = [])
 {
     $config = empty($config) ? Config::get('geetest') : $config;
-    $geetest = new \think\geetest\GeetestLib($config);
-    Config::set('gt_user_id', $_SERVER['REQUEST_TIME']);
-    Config::set('gt_server_status', $geetest->pre_process(Config::get('gt_user_id')));
+    $geetest = new GeetestLib($config);
+    Session::set('gt_user_id', $_SERVER['REQUEST_TIME']);
+    Session::set('gt_server_status', $geetest->pre_process(Session::get('gt_user_id')));
     return $geetest->get_response_str();
 }
 
 /**
- *
+ * 获取地址
  * @return string
  */
 function geetest_url()
@@ -27,10 +25,6 @@ function geetest_url()
     return Url::build('/geetest');
 }
 
-/**
- *
- * @return bool
- */
 /**
  * 极验验证
  * @param array $post post提交的数据
@@ -40,9 +34,9 @@ function geetest_url()
 function geetest_check($post, $config = [])
 {
     $config = empty($config) ? Config::get('geetest') : $config;
-    $geetest = new \think\geetest\GeetestLib($config);
-    if (1 == Config::get('gt_server_status')) {
-        if ($geetest->success_validate($post['geetest_challenge'], $post['geetest_validate'], $post['geetest_seccode'], Config::get('gt_user_id'))) {
+    $geetest = new GeetestLib($config);
+    if (1 == Session::get('gt_server_status')) {
+        if ($geetest->success_validate($post['geetest_challenge'], $post['geetest_validate'], $post['geetest_seccode'], Session::get('gt_user_id'))) {
             return true;
         } else {
             return false;
@@ -55,11 +49,3 @@ function geetest_check($post, $config = [])
         }
     }
 }
-
-
-
-
-
-
-
-
